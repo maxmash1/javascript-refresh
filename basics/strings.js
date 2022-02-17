cb28ce2e2f7077fc501462fdd6f5ed199b500139
Mashrur Hossain <95499645+maxmash1@users.noreply.github.com>
2022-02-17 22:04:33 +0000

    Add basics strings exercise

---
+++ basics/strings.js
@@ -0,0 +1,17 @@
+let name = 'John';
+let myFirstName = 'Mashrur';
+let myLastName = "Hossain";
+let myAge = 40;
+console.log(myFirstName)
+console.log(myAge)
+console.log(`My name is ${myFirstName} and I am ${myAge} years old.`);
+let myFullName = `${myFirstName} ${myLastName}`;
+console.log(myFullName);
+console.log(`My name is ${myFullName} and I am ${myAge} years old.`);
+// Using concatenation operator +
+console.log(myFirstName + " " + myLastName);
+let myCity = "New York";
+let myState = "New York";
+let myCountry = "USA";
+let myLocation = `${myCity}, ${myState}, ${myCountry}`;
+console.log(myLocation);
