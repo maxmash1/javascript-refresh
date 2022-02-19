5254a78a9b23ad0b28a657ee6c73672074c4c3d1
Mashrur Hossain <95499645+maxmash1@users.noreply.github.com>
2022-02-19 21:43:41 +0000

    Add numbers playground

---
+++ basics/numbers.js
@@ -0,0 +1,14 @@
+let num = 10;
+let num2 = 20;
+let num3 = num + num2;
+let num4 = 7.5 - -8
+let x = 10;
+let num5 = (x + 1) * 2
+let age = 40;
+let dogYears = (age + 1) / 7;
+console.log(dogYears);
+
+let studentScore = 18
+let maxScore = 20
+let percent = (studentScore / maxScore) * 100
+console.log(percent);
